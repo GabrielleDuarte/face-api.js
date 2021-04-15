43843a7ae67120353d39f13358a26b3862c25d00
duarte_ <gabrielleduartee3@gmail.com>
2021-04-14 21:16:42 -0300

    Adds age and gender recognition

--- examples/examples-browser/public/js/decodeFaceMatching.js
+++ examples/examples-browser/public/js/decodeFaceMatching.js
@@ -4,6 +4,7 @@ let isStop = false
 let faceMatcher = null
 let currImageIdx = 2, currClassIdx = 0
 let to = null
+let ageGenderData =null
 
 function onSlower() {
     interval = Math.min(interval + 100, 2000)
@@ -38,18 +39,37 @@ function displayImage(src) {
     getImg().src = src
 }
 
+
+//const detectionsWithAgeAndGender = await faceapi.detectAllFaces(input).withFaceLandmarks().withAgeAndGender()
 async function runFaceRecognition() {
     async function next() {
+    $('#age').val('-')
+    $('#expression').val('-')
     const input = await faceapi.fetchImage(getFaceImageUri(classes[currClassIdx], currImageIdx))
     const imgEl = $('#face').get(0)
     imgEl.src = input.src
 
     const ts = Date.now()
-    const descriptor = await faceapi.computeFaceDescriptor(input)
+    const descriptor = await faceapi.computerFaceDescription(input)
     displayTimeStats(Date.now() - ts)
 
     const bestMatch = faceMatcher.findBestMatch(descriptor)
+    const detectionsWithAgeAndGender = await faceapi.detectAllFaces(input).withFaceLandmarks().withAgeAndGender().withFaceExpressions()
+
     $('#prediction').val(bestMatch.toString())
+
+    const canvas = $('#overlay').get(0)
+    faceapi.matchDimensions(canvas, ImgEl)
+
+    const resizedResults = faceapi.resizeResults(detectionsWithAgeAndGender, input)
+    const minConfidence = 0.05
+    resizedResults.forEach(detectionsWithAgeAndGender => {
+        const{ age, gender, genderProbability, expressions} = result
+        $('#age').val(`${faceapi.utils.round(age,0)} years / ${gender} (${faceapi.utils.round(genderProbability)})`)    
+        $('expression').val(`${JSON.stringify(expressions)}`)
+    })
+
+    faceapi.draw.drawDetections(canvas, resizedResults, minConfidence)
 
     currImageIdx = currClassIdx === (classes.length - 1)
         ? currImageIdx + 1
@@ -61,24 +81,30 @@ async function runFaceRecognition() {
     }
     await next(0, 0)
 }
-async function run() {
 
+  async function run() {
     try {
-        setStatusText('loading model file...')
+      setStatusText('loading model file...')
 
-        await faceapi.loadFaceRecognitionModel('/')
+    await faceapi.loadFaceRecognitionModel('/')
 
-        setStatusText('computing initial descriptors...')
+    // load face detection and age and gender recognition models
+    // and load face landmark model for face alignment
+    await changeFaceDetector(SSD_MOBILENETV1)
+    await faceapi.loadFaceLandmarkModel('/')
+    await faceapi.nets.ageGenderNet.load('/')
+    await faceapi.loadFaceExpressionModel('/')
 
-        faceMatcher = await createBbtFaceMatcher(1)
-        $('#loader').hide()
+      setStatusText('computing initial descriptors...')
 
-        runFaceRecognition()
+      faceMatcher = await createBbtFaceMatcher(1)
+      $('#loader').hide()
+
+      runFaceRecognition()
     } catch (err) {
-        console.error(err)
+      console.error(err)
     }
-    }
+  }
 
 
-
-
+    
